793750c5042b6a0267382f13ffeff09609b38c48
James Bravender-Coyle <jbravendercoyle@gmail.com>
2017-07-02 00:46:26 +1000

    - added InventoryPanel script to the menu inventory to track items - shows item name and quantity - added Potion

---
+++ Assets/Scripts/ItemScripts/Potion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potion : MonoBehaviour {
+
+	public string ItemName;
+	public int quantity;
+
+	private float healValue;
+
+	private string description;
+
+	// Use this for initialization
+	void Awake () {
+		ItemName = "Potion";
+		healValue = 50f;
+		description = "A Fast-Acting Health Potion. Heals you up real good. (For 50HP)";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/MenuScripts/InventoryPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryPanel : MonoBehaviour {
+
+	string ItemName;
+	int quantity;
+
+	private GameObject Inventory;
+	private Transform Item1;//Item 1
+
+	// Use this for initialization
+	void Start () {
+		Page1 ();
+	}
+	
+	public void Page1()
+	{ 
+		//Find Player Inventory
+		Inventory = StartBattle.PartyManager.transform.Find("Inventory").gameObject;
+
+		//Get Items in Inventory
+		ItemName = Inventory.transform.GetChild(0).gameObject.GetComponent<Potion>().ItemName;
+		quantity = Inventory.transform.GetChild (0).gameObject.GetComponent<Potion> ().quantity;
+
+		//Set Info in Inventory
+		Item1 = this.gameObject.transform.GetChild(0).gameObject.transform;
+		Item1.GetComponent<Text>().text = (ItemName + " " +quantity);
+	}
+}
